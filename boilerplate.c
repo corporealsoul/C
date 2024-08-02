2ffe9ebdff71724233ab55ce70a59540c1370616
ONOSENDAI\uniqs <bca.anup@gmail.com>
2024-08-02 09:55:20 +0530

    c

---
+++ boilerplate.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+// Function declarations
+void exampleFunction();
+
+// Main function
+int main() {
+    // Print a welcome message
+    printf("Hello, World!\n");
+
+    // Call an example function
+    exampleFunction();
+
+    // Return 0 to indicate successful execution
+    return 0;
+}
+
+// Example function definition
+void exampleFunction() {
+    // Print a message from the example function
+    printf("This is an example function.\n");
+}
